0fa200bf884e5439196f7e174ceb51ff16c4e779
Ellie2427 <klaravielleporcadilla@gmail.com>
2026-08-11 08:37:42 +0800

    08/11/26

---
+++ 1st Sem/CC102.1/08-11-26 Lecture 2/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("My name is Dominic Anton D. Siao");
+    }
+}
