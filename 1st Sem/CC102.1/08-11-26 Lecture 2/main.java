fd16ff4bc8947c12869abd9e9cc2162200eb393c
Akiratorizawa <siao1valo@gmail.com>
2026-08-11 08:47:25 +0800

    08/11/26 student profile

--- 1st Sem/CC102.1/08-11-26 Lecture 2/main.java
+++ 1st Sem/CC102.1/08-11-26 Lecture 2/main.java
@@ -1,6 +1,26 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-        System.out.println("My name is Dominic Anton D. Siao");
+        System.out.println("--------------------------------------------");
+        System.out.println("|               STUDENT PROFILE            |");
+        System.out.println("--------------------------------------------");
+
+        String firstName = "Dominic Anton";
+        String lastName = "Siao";
+
+        String address = "Tabaco City, Albay";
+
+        String course = "BS Computer Science";
+
+        int age = 18;
+
+        String hobby = "going to the gym, studying, and playing video games";
+
+        System.out.println("Hello, Everyone!");
+        System.out.println("My name is " + firstName + " " + lastName);
+        System.out.println("I live in " + address);
+        System.out.println("My course is " + course);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My hobby is " + hobby + ".");
+
     }
 }
