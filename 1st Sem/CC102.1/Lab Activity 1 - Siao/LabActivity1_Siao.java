687fc553515394d6ffc5771c920d76ebccd3f080
Akiratorizawa <siao1valo@gmail.com>
2026-08-18 09:22:33 +0800

    lab activity 1 lecture 3

--- 1st Sem/CC102.1/Lab Activity 1 - Siao/LabActivity1_Siao.java
+++ 1st Sem/CC102.1/Lab Activity 1 - Siao/LabActivity1_Siao.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CanteenActivity {
+public class LabActivity1_Siao {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("============================================");
