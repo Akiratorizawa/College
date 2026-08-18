9a1fdc13c3f2313c8d6addd950dc3fe2cc336fc2
Akiratorizawa <siao1valo@gmail.com>
2026-08-18 08:22:36 +0800

    arithmetic example lecture 3

---
+++ 1st Sem/CC102.1/08-18-26 Lecture 3/ArithmeticExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("");
+        
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        input.close();
+    }
+}
