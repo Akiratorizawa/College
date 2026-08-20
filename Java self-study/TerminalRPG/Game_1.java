fdba82f9b23521343a5e8b1d21af57e0b3f04e6a
Akiratorizawa <siao1valo@gmail.com>
2026-08-20 22:34:10 +0800

    terminal barebones pokemon battle sim

---
+++ Java self-study/TerminalRPG/Game_1.java
@@ -0,0 +1,13 @@
+package TerminalRPG;
+
+import java.util.Scanner;
+
+import static TerminalRPG.Helpers.mainMenu;
+
+public class Game_1 {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        mainMenu();
+
+    }
+}
